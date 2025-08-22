9538bf51a5e5c81b98be412a799820f61d3cdcea
elizaveta-tagirova <elizaveta.tagirova@quantori.com>
2025-08-22 11:16:00 +0500

    add files with named blocks markers

---
+++ _build.gradle.kts
@@ -0,0 +1,13 @@
+plugins { /* … */ }
+
+// >>> BEGIN GENERATED (some-block) DO NOT EDIT
+// source: <>
+// checksum: <>
+// <<< END GENERATED
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+// rest of file...
