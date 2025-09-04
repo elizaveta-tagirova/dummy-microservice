bde0f9aa1259c9befbd1ceee40a6455949ec0bd1
Config Injector <config-injector@example.com>
2025-09-04 23:48:07 +0000

    chore(config): inject .config/generated

--- _build.gradle.kts
+++ _build.gradle.kts
@@ -18,6 +18,7 @@ param="param value 2"
 // <<< END GENERATED
 // <<< END GENERATED
 // <<< END GENERATED
+// <<< END GENERATED
 
 java {
     toolchain {
--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,7 @@ param="param value 1"
 // <<< END GENERATED
 // <<< END GENERATED
 // <<< END GENERATED
+// <<< END GENERATED
 
 java {
     toolchain {
